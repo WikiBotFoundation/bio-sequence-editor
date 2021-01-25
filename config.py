7391fee6d9d1bcb68a495a300a8a56774b1a3940
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-25 20:57:21 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+# files created by Wikibot foundation/WikiBot
+# This is a github page
+  {        
+       Config.local(data.py) 
+
+    From(A-SCRIPT.py-data(pdbdata.confirm.pdb)
+    From(A-SCRIPF.py-data(filetype:pdb.local)
+   pdb.extractor(pdbfiletype.localdata(pdb.confirm.data
+     
+         } 
+      pdb extractor.sequence(ButtonView.folder(buttonfolder.(buttonfolder/buttons/SaveData.html
+    Savedatafrom.pdb(Id).local(data.c)
+         data.c(extractor.id(local.c.ViewFor.folder(data.[],[confirm.data.py(sequence.pdb(objects.data(3d
+      Data(object.cpp(c.id
+                    sequence.data(ATOM:Extraxtpdb.atomview(local.ansoiu.data(pdb.file.data[confirm.data]
+   Extract; local.cpp(extractor.atomsequence.view(lexer="sequence.[], biopython.data(cpp.file)
+   Extract; launcher.cpp(extractor.atomsequence.view="3dsoftware.html(local.biohtml).3d
+        Extract.data(from.id(local.cpp.sequence.position="3D-sequence(modeller.pdb(installscript.data(PyData.local(script.3dsequence(data)
+        
+    Extractor.cpp(local.id(form(@$script)
+    Extractor.cpp(local.id(form(launcher.programming3d="scripr
+            3dobject.helix(helix(position="3dline"
+          Mydata.object(3d.helix(local.c).atom.py
+     Extrator.mydata(script.3d(form(blend.blenderdata(script.position="helix"
+             Confirm.data(script.blend(atom.py.sequence(script)
+        Github.data(script == data.3d(blend.helixline.(
+    Betasheets="position="100".helix.local(data.atom.py.atomline(aminoacidsequence.k)
+    Betasheets="atom.(s_n_o_h_c).helixdata.extractor.pdb.sequence(aminoacid.k)
+        Mydata="atom.(p_n_o_h_c).dna == RNA.structure.pdb(config.data.RNA == DNA
+           
+    
