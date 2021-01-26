08d740b80f6c7054e598d0bfdae01c05037f66f2
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-26 23:24:30 +0300

    ModellerFuction

---
+++ cs/ModellerFuction.cs
@@ -0,0 +1,21 @@
+// Script Loaded //
+        Script.public(data[machine.data(script.cs
+
+public Void Script == clients.data(pdb.protein.data.bank(script)
+          Public.(script.localdata[pdb{isProteinData}.[script.data(script.github
+    public Void == pdb.protein == data bank
+             
+  Void(.class == pdb.script(proteindatabank,{pdb.script}
+  Void(.class == pdb.scriptsequence.(databank{pdb.localhost:000, data
+         public Void checkbox.data(script.pdb(is data
+  SqlViewer(script.void(checkbox.proteindatabank.nav
+             Void(.class.checkData.nav(script.protein.databank
+               
+    MySQL(.script.data(bank.checkTheData.nav(proteindatabank)
+       
+                    public data (script.pdb(proteindatabank.atom(structure.checkData)
+         CheckAtom.ip(script.atom(pdb.proteindatabank == client.pub(script).protein)
+    Cs.script(Atom.ip(structure.id(pdb.databank(rscb.id(client.id(script.data
+
+    
+
