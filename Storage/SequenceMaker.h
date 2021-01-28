ed6e71a8667986401bde659af82622c549e7cedf
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-28 22:35:44 +0300

    File

---
+++ Storage/SequenceMaker.h
@@ -0,0 +1,15 @@
+#indline script
+#This is a Source Code
+#This is a function
+       
+  struct data script == IsonData
+  struct data script == C.py(script,local,.c)
+         struct
+   ViewerPy(Python = true)
+   Mol(script_option.code(source.script = true)
+   Mol(sctipt_option.code(molviewer.2D = false)
+        python.(extension(code.molviewer.c(data.script.)
+              
+  MolScript3D(3dviewerdata_script3D.(localextension.js(c)
+  MolScript2D(2Dviewerdata_local.player(structure.c
+          
