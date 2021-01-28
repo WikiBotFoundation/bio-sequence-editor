b0fc85479a1291716fee9c97d8e3eab702ee17d1
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-28 22:52:45 +0300

    structure viewer

---
+++ Storage/structure.py
@@ -0,0 +1,33 @@
+#indline_script.data
+#script_protect.data
+#python_protect.data
+        # protect the data #
+   script(protection.cs(data.script = true)
+      set Var="script.data"
+              struct="java/cs"
+       cpp.data.cs(script == protection)
+          GhIshFGJP-IJhksKSHMJmK.key == structure
+       KeyData="protein"
+             GKB$&82IHkGeSQkwKL.key == structure
+       KeyData="XRAY MONITOR PROTEIN"
+               Monitor.cs(button == engine(data)
+                Monitor.but(script == engine(data, KeyAPI., Protein)
+         Monitor.cs(SetExtension == monitor structure
+                 Set Protein(data)
+                          Monitor == structure(n_o_s_c_cı_p)
+                         Antimonitor == viewer.data(n_c_o_c_s)
+        File.api(script/java)
+                protein(antimonitor.n_sc(sulface).key
+                api.data(script.monitor(api).key
+      Proteinapi_script.data(monitor.key(api)
+   Protein_monitor.script(java.key(data)
+ Monitor_script(api.source.key(protein).key(plb)
+Script source.(dataset.structure)
+     
+ 
+       
+
+          
+   
+  
+    
