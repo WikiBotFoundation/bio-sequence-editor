18620a0a43704ca2b3c39efa63e1029920b9901a
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-30 21:49:24 +0300

    Protein Module

---
+++ ProteinModule.py
@@ -0,0 +1,38 @@
+# This is A proteinal Modules #
+# This is A Monitor structure #
+       <script.py(data)
+   struct(data.if(yourdata.module.(structure
+      Demoprotein.(protein).module
+               Python.script(module.structure.ssdd
+         Monitor_script.monitor.
+       Monitor.data(script.data_monitor {protein}.benzene
+        *$02928888*-Benzene.monitor(cs)
+     Script val(100).monitor.(molecule)
+         Molecule(val(.100)
+                  Script.molecule.json(editor.js)
+        PyMol.ketcher.(data.struct.
+              Monitor.(val(100)
+                           json.(script.ifData).json
+      Molprotein.Struct-3dview.ifData.json(j)
+             Json.data(script.json.(json.script.[is data{local
+& Sketch mol: Data_script.monitor
+& Sketch mol: Benzene.data(benzene)
+& Sketch mol: Benzene.data(benzene)
+& Sketch mol: Benzene.data(benzene)
+& Sketch mol: Benzene.data(benzene)
+& Sketch mol: json(addto(.script(benzene:o
+& Sketch mol: json.addnitro(script(benzene:o_n_c
+& Sketch mol: (c="carbon") molscript.mol(carbon)
+& Sketch mol: carbon line="script.n(_____)
+& Sketch mol: proteinline.helixbased(___cs).structure.ff)
+& Sketch mol: C="carbon_cs(carbon.n_s.gli(c(allval(100)
+& Sketch atom: C=carbon.cs(val(protein.n(o_c_o_c.editval(isprotein)
+& Sketch Proteinline: protein.val(!struc.o_c_n(script.molecule.peptice
+
+
+
+
+
+
+
+  
