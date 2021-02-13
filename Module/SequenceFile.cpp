370e15b4e72630b424e53191386a1f36f9431e5b
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-13 12:58:30 +0300

    Add files via upload

---
+++ Module/SequenceFile.cpp
@@ -0,0 +1,33 @@
+#include <bio>
+#include <sequence>
+#include <file>
+#include <pdb>
+    const.namespace(key)
+      bot(myPy) == file
+         bio.sequence(haHgIUeyoN)
+      namespace == bot.keyword
+          bioSequence.key
+  [[[bio.mesh(pdb))]]]
+  [[[pdb:file/system]]]
+  [[myPy:pdb/system]]
+      {{{{ mesh.name(mesh-1.000.000)}}}}
+     private system namespace == folder
+        pdb.sequence(3D.file)
+      javascript.keyword( == 1.000.0000)
+    system void 
+           PyController == keyword
+    bot.system 
+            mesh.file(namespace).mesh
+    pdb.file(sequence.local) == keyword
+             pdbmod
+         private system controller.mesh
+      x,y,z.file ( == mesh="1.00000")
+     3djava ( == mesh.lc)
+             namespace.keyword(sequence.id)
+          3Dview.proteinFile(name.py)
+       namespace(pdb.file(coordinates)
+        pdbb.file/localtest;000
+        pdb.file/localtest;000
+      pdb.file(view == keyword)
+          pdb.file(== 3D)
+             object.helix == mesh
