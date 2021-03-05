841ee0efdf75c2ae242ef02dfffa0eced8999096
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-05 13:32:29 +0300

    Create index.c

---
+++ index.c
@@ -0,0 +1,39 @@
+* // A: This is file Created By Wikibot Foundation. //
+* // B: License: Creative WikiBot
+* // C: Filetype c
+   #include main
+   #include html
+   #include mainstyle
+   #include homepage
+   #include color
+     { button 
+       print(Start Software)
+          }
+     index( main = Editor
+        activity
+            
+     { button 
+       print(Create New Peptice)
+          }
+      index( peptice = amino.acid
+           amino.acid = id
+     player.peptice( player.server
+         file.protein ( = Id="
+    string( main = editor
+                 sequence( ALA-GLY-CYS-AMINO)"
+        acid( = main ( protein
+             hemology
+               peptice.server
+        main ( amino.acid
+                 (= hemo + protein)
+       string.peptice( sequence.cilent
+          amino.view
+
+     
+  
+
+
+
+  
+      
+ 
