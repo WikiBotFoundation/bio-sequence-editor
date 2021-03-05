f90f410b6f4ca76357856b4431d362cd1f0c2e13
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-03-05 12:40:15 +0300

    Add files via upload

---
+++ string.c
@@ -0,0 +1,72 @@
+#include helloworld
+#include biology
+#include sequence
+   struct = biology( mash.file)
+    struct.peptice( protein = mash)
+   molecule(  ALA-GLY-CYS-Amino)
+      peptide(  = start.amino_acid
+   [[bio.mesh([ nan_peptice] = s
+   [[bio.mesh([ nan_peptice] = n
+   [[bio.mesh([ nan_peptice] = o
+   [[bio.mesh([ nan_peptice] = c
+   [[bio.mesh([ nan_peptice] = h
+       struct.amino( peptide + sequence
+    import.fasta([ view.fasta
+      button
+       print(Upload Fasta)
+     fasta.sequence( void + visualize
+       fasta.id ( void + plus)
+    data.txt( import.void
+         fasta = sequence
+       protein(  void.dna
+   public.void ( = import.dna
+          fasta( = file
+            fasta( A-B-H-H)
+     amino.seq( = dna
+             picture.dna( = abort.dna
+    void dna = seq ( chain.id
+        Id( dna = ( fasta
+      view( file.fasta == visual.seq
+         git(; biology
+       import
+       sequence.visual
+       sequence.fasta
+            void ( = sequence
+        view.file ( txt = import.chain( id
+     dna.system ( = abort.sequence)  
+     chain.id ( void seq)
+          molecule.id ( void = chain)
+      fasta.keyword( system.molecules)
+           view.txt( = _Abort_)
+       python( _ abort._)
+            sequence.file
+      abort.file
+           dna( == file.mesh
+      import.fasta( file)
+            fasta ( = view)
+        abort seq ( txt)
+           file.id 
+         dna( = import.dynamic_cast 
+     dynamic_dna
+         txt = ( file = seq)
+      abort.dna    
+          molecule.id = molecule
+    fasta="true"  
+        import.fasta( seq.node(js))
+    fasta = seq( abort.molecule 
+        // Protein Modelling System //
+     button
+      print(Create Peptice)
+         abort.sequence
+      data.id( == molecular)
+     button.box
+       print(ALA)
+          ala.abort
+       alanine_sequence ( dna)
+    modelling ( js)
+           sequence.visualize 
+        atom ( operator.dna)
+      modelling _ alanine ( operator.visualize)
+    string.box ( _ operator = sequence)
+         atom.alanine( molecular)
+       mesh( all.mouse( == new.peptice)
